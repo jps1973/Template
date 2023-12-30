d88a73743b46274e1e54f1941886aff3d632c47c
jps1973 <47426983+jps1973@users.noreply.github.com>
2023-12-30 20:50:08 +0000

    Updates to Common header file

--- Common.h
+++ Common.h
@@ -9,6 +9,9 @@
 #define ALL_FILES_FILTER														"*.*"
 #define NEW_LINE_TEXT															"\r\n"
 
+#define WINDOW_BORDER_WIDTH														::GetSystemMetrics( SM_CXBORDER )
+#define WINDOW_BORDER_HEIGHT													::GetSystemMetrics( SM_CYBORDER )
+
 #define ABOUT_MESSAGE_CAPTION													"About"
 #define ERROR_MESSAGE_CAPTION													"Error"
 #define INFORMATION_MESSAGE_CAPTION												"Information"
