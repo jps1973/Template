364aad4b6cd93f44837f2c227f6589fb51d7d57a
jps1973 <47426983+jps1973@users.noreply.github.com>
2025-08-28 17:03:48 +0100

    Corrected ShellExecute #defines

--- Common.h
+++ Common.h
@@ -14,13 +14,13 @@
 #define WINDOW_BORDER_HEIGHT													::GetSystemMetrics( SM_CYBORDER )
 
 #define SHELL_EXECUTE_OPEN_COMMAND												"open"
-#define SHELL_EXECUTE_MINIMUM_SUCCESS_RETURN_VALUE								32
+#define SHELL_EXECUTE_MAXIMUM_FAILURE_RETURN_VALUE								32
 // Note that you need to cast ShellExecute to an INT_PTR in order to use the above, e.g.
 //
 //		// Open item
-//		if( ( INT_PTR )ShellExecute( NULL, SHELL_EXECUTE_OPEN_COMMAND, lpszFilePath, NULL, NULL, SW_SHOWDEFAULT ) <= SHELL_EXECUTE_MINIMUM_SUCCESS_RETURN_VALUE )
+//		if( ( INT_PTR )ShellExecute( NULL, SHELL_EXECUTE_OPEN_COMMAND, lpszFilePath, NULL, NULL, SW_SHOWDEFAULT ) > SHELL_EXECUTE_MAXIMUM_FAILURE_RETURN_VALUE )
 //		{
-//			// Unable to open item
+//			// Successfully opened item
 
 #define UNABLE_TO_OPEN_FILE_ERROR_MESSAGE_FORMAT_STRING							"Unable to open '%s'"
 
